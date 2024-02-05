4b3c23f03056b7fbe0236feaa8f7461d7b3169d6
Francisco Zuniga <118315442+zunigafran@users.noreply.github.com>
2024-02-05 05:39:53 -0600

    uploading files

---
+++ seed.sql
@@ -0,0 +1,28 @@
+
+DROP DATABASE IF EXISTS  adopt;
+
+CREATE DATABASE adopt;
+
+\c adopt
+
+CREATE TABLE pets
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  species TEXT NOT NULL,
+  photo_url TEXT,
+  age INT,
+  notes TEXT,
+  available BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+INSERT INTO pets
+  (name, species, photo_url, age, notes, available)
+VALUES
+  ('Woofly', 'dog', 'https://www.what-dog.net/Images/faces2/scroll001.jpg', 3, 'Incredibly adorable.', 't'),
+  ('Porchetta', 'porcupine', 'http://kids.sandiegozoo.org/sites/default/files/2017-12/porcupine-incisors.jpg', 4, 'Somewhat spiky!', 't'),
+  ('Snargle', 'cat', 'https://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg', null, null, 't'),
+  ('Dr. Claw', 'cat', null, null, null, 't')
+  ('Amaterasu', 'dog', 'https://www.themillennialsahm.com/wp-content/uploads/2020/11/SuedeTitle.jpg', 1, 'So Cute!!', 't'),
+  ('Swampert', 'amphibian', 'https://t0.gstatic.com/images?q=tbn:ANd9GcTIKc_ebY9MXQDFMM8je1jCiwxAzccZPfC7x6wbC7DgZ5QnDsIPGB1KmNSjgCWAMBBR_4msIw', 3, 'Chilling', 'f');
+
